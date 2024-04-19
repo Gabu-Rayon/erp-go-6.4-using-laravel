a25b48ab402d258cedd5f0e7cf1f4296a9bd29d2
Christian Wamalwa <bobitlmr188221@spu.ac.ke>
2024-04-19 17:44:35 +0300

    Fix bug in code

--- app/Http/Controllers/GetItemInformationController.php
+++ app/Http/Controllers/GetItemInformationController.php
@@ -27,6 +27,72 @@ class GetItemInformationController extends Controller
         return view('iteminformation.create');
     }
 
+
+    // public function edit()
+    // {
+    //     return view('iteminformation.edit');
+        
+    // }
+    // public function getItemInformation()
+    // {
+
+    //     // Increase maximum execution time to 300 seconds (5 minutes)
+    //     ini_set('max_execution_time', 300);
+
+    //     $url = 'https://etims.your-apps.biz/api/GetItemInformation?date=20220409120000';
+
+    //     $response = Http::withHeaders([
+    //         'key' => '123456'
+    //     ])->get($url);
+
+    //     $data = $response->json();
+    //     $itemLists = $data['data']['data']['itemList'];
+
+    //     \Log::info('API Request Data: ' . json_encode($data));
+    //     \Log::info('API Response: ' . $response->body());
+    //     \Log::info('API Response Status Code: ' . $response->status());
+
+    //     if (isset($itemLists)) {
+    //         try {
+    //             foreach ($itemLists as $item) {
+    //                 ItemInformation::create([
+    //                     'tin' => $item['tin'],
+    //                     'itemCd' => $item['itemCd'],
+    //                     'itemClsCd' => $item['itemClsCd'],
+    //                     'itemTyCd' => $item['itemTyCd'],
+    //                     'itemNm' => $item['itemNm'],
+    //                     'itemStdNm' => $item['itemStdNm'],
+    //                     'orgnNatCd' => $item['orgnNatCd'],
+    //                     'pkgUnitCd' => $item['pkgUnitCd'],
+    //                     'qtyUnitCd' => $item['qtyUnitCd'],
+    //                     'taxTyCd' => $item['taxTyCd'],
+    //                     'btchNo' => $item['btchNo'],
+    //                     'regBhfId' => $item['regBhfId'],
+    //                     'bcd' => $item['bcd'],
+    //                     'dftPrc' => $item['dftPrc'],
+    //                     'grpPrcL1' => $item['grpPrcL1'],
+    //                     'grpPrcL2' => $item['grpPrcL2'],
+    //                     'grpPrcL3' => $item['grpPrcL3'],
+    //                     'grpPrcL4' => $item['grpPrcL4'],
+    //                     'grpPrcL5' => $item['grpPrcL5'],
+    //                     'addInfo' => $item['addInfo'],
+    //                     'sftyQty' => $item['sftyQty'],
+    //                     'isrcAplcbYn' => $item['isrcAplcbYn'],
+    //                     'rraModYn' => $item['rraModYn'],
+    //                     'useYn' => $item['useYn']
+    //                 ]);
+    //             }
+    //             return redirect()->back()->with('success', 'Item Information added successfully.');
+    //         } catch (\Exception $e) {
+    //             \Log::error('Error adding Item Information from the API: ' . $e->getMessage());
+    //             return redirect()->back()->with('error', 'Error adding Item Information from the API.');
+    //         }
+    //     } else {
+    //         return redirect()->back()->with('error', 'No data found in the API response.');
+    //     }
+    // }
+
+
     public function update(Request $request, ItemInformation $iteminformation)
     {
         try {
@@ -68,52 +134,64 @@ class GetItemInformationController extends Controller
     }
     public function getItemInformation()
     {
-
-        // Increase maximum execution time to 300 seconds (5 minutes)
+        //execution time to 300 seconds (5 minutes)
         ini_set('max_execution_time', 300);
 
         $url = 'https://etims.your-apps.biz/api/GetItemInformation?date=20220409120000';
 
         $response = Http::withHeaders([
             'key' => '123456'
-        ])->get($url);
+        ])->withOptions([
+                    'timeout' => 180,
+                ])->get($url);
+
 
         $data = $response->json();
         $itemLists = $data['data']['data']['itemList'];
-        
+
         \Log::info('API Request Data: ' . json_encode($data));
-        \Log::info('API Response: ' . $response->body());
         \Log::info('API Response Status Code: ' . $response->status());
 
         if (isset($itemLists)) {
             try {
-                foreach ($itemLists as $item) {
-                    ItemInformation::create([
-                        'tin' => $item['tin'],
-                        'itemCd' => $item['itemCd'],
-                        'itemClsCd' => $item['itemClsCd'],
-                        'itemTyCd' => $item['itemTyCd'],
-                        'itemNm' => $item['itemNm'],
-                        'itemStdNm' => $item['itemStdNm'],
-                        'orgnNatCd' => $item['orgnNatCd'],
-                        'pkgUnitCd' => $item['pkgUnitCd'],
-                        'qtyUnitCd' => $item['qtyUnitCd'],
-                        'taxTyCd' => $item['taxTyCd'],
-                        'btchNo' => $item['btchNo'],
-                        'regBhfId' => $item['regBhfId'],
-                        'bcd' => $item['bcd'],
-                        'dftPrc' => $item['dftPrc'],
-                        'grpPrcL1' => $item['grpPrcL1'],
-                        'grpPrcL2' => $item['grpPrcL2'],
-                        'grpPrcL3' => $item['grpPrcL3'],
-                        'grpPrcL4' => $item['grpPrcL4'],
-                        'grpPrcL5' => $item['grpPrcL5'],
-                        'addInfo' => $item['addInfo'],
-                        'sftyQty' => $item['sftyQty'],
-                        'isrcAplcbYn' => $item['isrcAplcbYn'],
-                        'rraModYn' => $item['rraModYn'],
-                        'useYn' => $item['useYn']
-                    ]);
+                //batch size
+                $batchSize = 100;
+
+                // Calculatebatches
+                $totalItems = count($itemLists);
+                $totalBatches = ceil($totalItems / $batchSize);
+
+                // Process items in batches
+                for ($i = 0; $i < $totalBatches; $i++) {
+                    $batchItems = array_slice($itemLists, $i * $batchSize, $batchSize);
+                    foreach ($batchItems as $item) {
+                        ItemInformation::create([
+                            'tin' => $item['tin'],
+                            'itemCd' => $item['itemCd'],
+                            'itemClsCd' => $item['itemClsCd'],
+                            'itemTyCd' => $item['itemTyCd'],
+                            'itemNm' => $item['itemNm'],
+                            'itemStdNm' => $item['itemStdNm'],
+                            'orgnNatCd' => $item['orgnNatCd'],
+                            'pkgUnitCd' => $item['pkgUnitCd'],
+                            'qtyUnitCd' => $item['qtyUnitCd'],
+                            'taxTyCd' => $item['taxTyCd'],
+                            'btchNo' => $item['btchNo'],
+                            'regBhfId' => $item['regBhfId'],
+                            'bcd' => $item['bcd'],
+                            'dftPrc' => $item['dftPrc'],
+                            'grpPrcL1' => $item['grpPrcL1'],
+                            'grpPrcL2' => $item['grpPrcL2'],
+                            'grpPrcL3' => $item['grpPrcL3'],
+                            'grpPrcL4' => $item['grpPrcL4'],
+                            'grpPrcL5' => $item['grpPrcL5'],
+                            'addInfo' => $item['addInfo'],
+                            'sftyQty' => $item['sftyQty'],
+                            'isrcAplcbYn' => $item['isrcAplcbYn'],
+                            'rraModYn' => $item['rraModYn'],
+                            'useYn' => $item['useYn']
+                        ]);
+                    }
                 }
                 return redirect()->back()->with('success', 'Item Information added successfully.');
             } catch (\Exception $e) {
@@ -133,7 +211,9 @@ class GetItemInformationController extends Controller
     // }
 
 
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
+
     }
     /**
      * Using Api Endpoint
