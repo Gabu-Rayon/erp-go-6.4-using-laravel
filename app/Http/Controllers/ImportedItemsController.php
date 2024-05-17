7cbb677eaf0117658df2055fc5b97fc596c6ea55
Gabu-Rayon <gibsonmurimi4@gmail.com>
2024-05-17 17:58:56 +0300

    Commit Changes

--- app/Http/Controllers/ImportedItemsController.php
+++ app/Http/Controllers/ImportedItemsController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\ImportedItems;
-use App\Models\UpdateImportItems;
 use Illuminate\Http\Request;
+use App\Models\ImportedItems;
+use App\Models\ItemInformation;
+use App\Models\ImportItemStatusCode;
+use Illuminate\Support\Facades\Http;
 
 class ImportedItemsController extends Controller
 {
@@ -26,8 +28,10 @@ class ImportedItemsController extends Controller
      */
     public function create()
     {
-        $updateImportedItems = UpdateImportItems::all();
-        return view('importeditems.create', compact('updateImportedItems'));
+        $importedItems = ImportedItems::all()->pluck('itemName', 'taskCode');
+        $items = ItemInformation::all()->pluck('itemNm', 'itemCd');
+        $importItemStatusCode = ImportItemStatusCode::all()->pluck('code', 'id');
+        return view('updateimportitem.create', compact('importedItems', 'items', 'importItemStatusCode'));
     }
 
     public function cancel(ImportedItems $importedItem)
@@ -40,7 +44,62 @@ class ImportedItemsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            \Log::info('IMPORTED ITEMS DATA');
+            \Log::info($request->all());
+
+            $importItem = ImportedItems::where('taskCode', $request['importedItemName'])->first();
+            $givenItem = ItemInformation::where('itemCd', $request['item'])->first();
+            $srNo = $importItem->srNo;
+            $taskCode = $request['importedItemName'];
+            $declarationDate = $importItem->declarationDate;
+            $itemSeq = $importItem->itemSeq;
+            $hsCode = $importItem->hsCode;
+            $itemCd = $givenItem->itemCd;
+            $itemClsCd = $givenItem->itemClsCd;
+            $importItemStatusCode = $request['importItemStatusCode'];
+            $occurredDate = $importItem->occurredDate;
+            $remark = $request['remark'];
+
+            $url = 'https://etims.your-apps.biz/api/MapImportedItem';
+
+            $response = Http::withHeaders([
+                'key' => '123456',
+                'accept' => '*/*',
+                'Content-Type' => 'application/json'
+            ])->post($url, [
+                        'importItemStatusCode' => $request['importItemStatusCode'],
+                        'declarationDate' => $declarationDate,
+                        'occurredDate' => $occurredDate,
+                        'remark' => $remark,
+                        'importedItemLists' => [
+                            [
+                                'taskCode' => $request['importedItemName'],
+                                'itemCode' => $request['item'],
+                            ]
+                        ]
+                    ]);
+
+            \Log::info('IMPORTED ITEMS API RESPONSE');
+            \Log::info($response);
+
+            UpdateImportItems::create([
+                'srNo' => $srNo,
+                'taskCode' => $taskCode,
+                'declarationDate' => $declarationDate,
+                'itemSeq' => $itemSeq,
+                'hsCode' => $hsCode,
+                'itemClassificationCode' => $itemClsCd,
+                'itemCode' => $itemCd,
+                'importItemStatusCode' => $importItemStatusCode
+            ]);
+
+            return redirect()->back()->with('success', 'Item Added Successfully');
+        } catch (\Exception $e) {
+            \Log::info('IMPORTED ITEMS ERROR');
+            \Log::info($e);
+            return redirect()->back()->with('error', 'Something Went Wrong');
+        }
     }
 
     /**
@@ -75,4 +134,67 @@ class ImportedItemsController extends Controller
     {
         //
     }
+
+    public function GetImportedItemInformation()
+    {
+        try {
+            ini_set('max_execution_time', 300);
+            $url = 'https://etims.your-apps.biz/api/GetImportedItemInformation?date=20220409';
+
+            $response = Http::withHeaders([
+                'key' => '123456'
+            ])->get($url);
+
+            $data = $response->json()['data'];
+
+            \Log::info('API  Data Request for getting Imported Item Information : ' . json_encode($data));
+            \Log::info('API Body Data Request for getting Imported Item Information: ' . $response->body());
+            \Log::info('API Response Status Code: ' . $response->status());
+
+            if (isset($data['data'])) {
+                try {
+                    foreach ($data['data']['itemClsList'] as $importedItem) {
+                        ImportedItems::create([
+                            'srNo' => $importedItem['srNo'],
+                            'taskCode' => $importedItem['taskCode'],
+                            'itemName' => $importedItem['itemName'],
+                            'hsCode' => $importedItem['hsCode'],
+                            'pkgUnitCode' => $importedItem['pkgUnitCode'],
+                            'netWeight' => $importedItem['netWeight'],
+                            'invForCode' => $importedItem['invForCode'],
+                            'declarationDate' => $importedItem['declarationDate'],
+                            'orginNationCode' => $importedItem['orginNationCode'],
+                            'qty' => $importedItem['qty'],
+                            'supplierName' => $importedItem['supplierName'],
+                            'nvcfcurExcrt' => $importedItem['nvcfcurExcrt'],
+                            'itemSeq' => $importedItem['itemSeq'],
+                            'exprtNatCode' => $importedItem['exprtNatCode'],
+                            'qtyUnitCode' => $importedItem['qtyUnitCode'],
+                            'agentName' => $importedItem['agentName'],
+                            'declarationNo' => $importedItem['declaratioNo'],
+                            'package' => $importedItem['package'],
+                            'grossWeight' => $importedItem['grossWeight'],
+                            'invForCurrencyAmount' => $importedItem['invForCurrencyAmount'],
+                            'status' => $importedItem['status']
+                        ]);
+                    }
+                    // return redirect()->back()->with('success', 'Item Information added successfully.');
+                    return redirect()->route('importeditems.index')->with('success', __('Imported Items  Created successfully .'));
+                } catch (\Exception $e) {
+                    \Log::error('Error adding Imported Items from the API: ' );
+                    \Log::error($e);
+                    // return redirect()->back()->with('error', 'Error adding Item Information from the API.');
+                    return redirect()->route('importeditems.index')->with('error', __('Error adding Imported Items from the API.'));
+                }
+            } else {
+                // return redirect()->back()->with('error', 'No data found in the API response.');
+                return redirect()->route('importeditems.index')->with('error', __('No data found in the API response.'));
+            }
+        } catch (\Exception $e) {
+            \Log::error($e);
+            // return redirect()->back()->with('error', 'Error adding Item Information from the API.');
+            return redirect()->route('importeditems.index')->with('error', __('Error adding Imported Items from the API.'));
+        }
+    }
+
 }
