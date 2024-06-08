8a3a721e23cb563c04ccdd621f9586af9b2888d5
Gabu-Rayon <gibsonmurimi4@gmail.com>
2024-06-08 12:28:00 +0300

    Commit Changes made

--- database/migrations/2024_05_18_184945_create_venders_table.php
+++ database/migrations/2024_05_18_184945_create_venders_table.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateVendersTable extends Migration
+
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -62,4 +63,4 @@ class CreateVendersTable extends Migration
     {
         Schema::dropIfExists('venders');
     }
-}
+};
