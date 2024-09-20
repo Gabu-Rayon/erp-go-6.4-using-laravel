5034989f908a82c9c5b01c0ebf948c5e6f47a949
chrisdroid <bobitlmr188221@spu.ac.ke>
2024-09-20 11:32:43 +0300

    Fix bug in code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,9 +1,11 @@
 <?php
+
 namespace Database\Seeders;
 
 use App\Models\Utility;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Request;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,28 +20,20 @@ class DatabaseSeeder extends Seeder
         Artisan::call('module:migrate LandingPage');
         Artisan::call('module:seed LandingPage');
 
-        if(\Request::route()->getName()!='LaravelUpdater::database')
-        {
-            // $this->call(PlansTableSeeder::class);
-            // $this->call(UsersTableSeeder::class);
-            // $this->call(AiTemplateSeeder::class);
-            $this->call(ProductServicesPackagingUnitsSeeder::class);
-            $this->call(ProductServiceUnitsSeeder::class);
-            $this->call(ImportItemStatusCodesSeeder::class);
-            $this->call(InvoiceStatusCodesSeeder::class);
-            $this->call(PaymentTypesCodeSeeder::class);
-            $this->call(PurchaseStatusCodesSeeder::class);
+        $this->call(PlansTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(AiTemplateSeeder::class);
+        $this->call(ProductServicesPackagingUnitsSeeder::class);
+        $this->call(ProductServiceUnitsSeeder::class);
+        $this->call(ImportItemStatusCodesSeeder::class);
+        $this->call(InvoiceStatusCodesSeeder::class);
+        $this->call(PaymentTypesCodeSeeder::class);
+        $this->call(PurchaseStatusCodesSeeder::class);
 
-            $this->call(PurchaseTypesCodeSeeder::class);
-            $this->call(SalesTypeCodesSeeder::class);
-            $this->call(CreditNoteReasonsSeeder::class);
-            $this->call(ItemTypeSeeder::class);
-            $this->call(StockReleaseTypesSeeder::class);
-
-        }else{
-            Utility::languagecreate();
-
-        }
-
+        $this->call(PurchaseTypesCodeSeeder::class);
+        $this->call(SalesTypeCodesSeeder::class);
+        $this->call(CreditNoteReasonsSeeder::class);
+        $this->call(ItemTypeSeeder::class);
+        $this->call(StockReleaseTypesSeeder::class);
     }
 }
